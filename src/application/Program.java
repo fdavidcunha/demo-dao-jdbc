68c24cc485e9250735c21e4ba35b403822343177
David <f.david.cunha@gmail.com>
2021-06-18 07:38:46 -0300

    Criação da classe Department

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,11 +3,15 @@ package application;
 import java.sql.Connection;
 
 import db.DB;
+import model.entities.Department;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Connection conn = DB.getConnection();
-		DB.closeConnection();
+		//Connection conn = DB.getConnection();
+		//DB.closeConnection();
+		
+		Department obj = new Department(1, "Teste");
+		System.out.println(obj);
 	}
 }
